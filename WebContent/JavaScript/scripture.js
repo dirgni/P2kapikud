cf64bedfbeee590d1d886edc7667b596e2cdfec9
dirgni <inx116@hotmail.com>
2014-03-17 04:13:40 +0200

    CSS, veidi HTML, paar JS rida..

--- WebContent/JavaScript/scripture.js
+++ WebContent/JavaScript/scripture.js
@@ -1,8 +1,9 @@
-
 function makeVisible(Id){
 	document.getElementById(Id).style.display="block";
+	$("body").css("overflow", "hidden");
 }
 
 function makeHidden(Id){
 	document.getElementById(Id).style.display="none";
+	$("body").css("overflow-y", "scroll");
 }
