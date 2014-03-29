7b242e32a1788293c8e22de3cf35724f3bc1384f
dirgni <inx116@hotmail.com>
2014-03-29 19:03:38 +0200

    Sortimine tabel.html'is

--- WebContent/JavaScript/scripture.js
+++ WebContent/JavaScript/scripture.js
@@ -1,3 +1,15 @@
+$(document).ready(function(){
+     $('#uudised_tabelis')
+     		.tablesorter({
+		    	 sortList: [[0,1],[1,1]],
+			     headers: {
+			    	 5: { sorter: false }
+			     },
+			     sortMultiSortKey: 'ctrlKey' 
+		     })
+//		     .tablesorter().tablesorterPager({container: $("#pager")});
+});
+
 function makeVisible(Id){
 	document.getElementById(Id).style.display="block";
 	$("body").css("overflow", "hidden");
